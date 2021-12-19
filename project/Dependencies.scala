f938650fceb7118c00d9ef3cedc7d45cb4ac71ed
Lucas Kjaero <lucas@lucaskjaero.com>
2021-12-19 14:44:13 +0100

    Don't pin scala version manually

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,8 +8,8 @@ object Dependencies {
   lazy val opencc4j = "com.github.houbb" % "opencc4j" % "1.7.1"
   lazy val cats = "org.typelevel" %% "cats-core" % "2.6.1"
   lazy val ws =
-    "com.typesafe.play" % "play-ahc-ws-standalone_2.13" % playWsStandaloneVersion
-  lazy val playJson = "com.typesafe.play" % "play-json_2.13" % "2.9.2"
+    "com.typesafe.play" %% "play-ahc-ws-standalone" % playWsStandaloneVersion
+  lazy val playJson = "com.typesafe.play" %% "play-json" % "2.9.2"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
 
   lazy val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.32"
